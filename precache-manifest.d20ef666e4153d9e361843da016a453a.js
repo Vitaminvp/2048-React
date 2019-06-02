313d4b68a6bd54b3bc88485f7ac82d34e58275de
Vitalii Vitaminenko <vitamin@ukr.net>
2019-06-02 20:22:21 +0300

    Updates

---
+++ precache-manifest.d20ef666e4153d9e361843da016a453a.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6fb6534081eecee8541c00b2a351d48a",
+    "url": "/2048-React/index.html"
+  },
+  {
+    "revision": "e23b1554f003f91696ea",
+    "url": "/2048-React/static/css/main.5c0fdbae.chunk.css"
+  },
+  {
+    "revision": "4510f47ba7c100bfa392",
+    "url": "/2048-React/static/js/2.93553d5f.chunk.js"
+  },
+  {
+    "revision": "e23b1554f003f91696ea",
+    "url": "/2048-React/static/js/main.532c06b5.chunk.js"
+  },
+  {
+    "revision": "80d531baf220e054f855",
+    "url": "/2048-React/static/js/runtime~main.b080f680.js"
+  },
+  {
+    "revision": "0d651f8ebfa508a5faea1e03c97f1a80",
+    "url": "/2048-React/static/media/clearsansbold.0d651f8e.woff2"
+  },
+  {
+    "revision": "3c8c6c940f1c1a9258dfad7e25347c21",
+    "url": "/2048-React/static/media/clearsansbold.3c8c6c94.ttf"
+  },
+  {
+    "revision": "f1d326af902254d77d541922ffe67beb",
+    "url": "/2048-React/static/media/clearsansbold.f1d326af.woff"
+  }
+]);
